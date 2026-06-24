48501cd49baf02f0fa1138ff7a8122787ecb6ed7
Julien Balsiere <julien.balsiere@gmail.com>
2026-06-24 10:32:01 +0200

    Exercice suite Fibonacci full ok

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let previous = 1;
+  let current = 1;
+  if (n === 1) {
+    return 1;
+  } else if (n === 0 || n === "0") {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  } else if (n < 0) {
+    return "OOPS"
+  } else {
+    for (let i = 3; i <= n; i++) {
+      let next = previous + current;
+      previous = current;
+      current = next;
+    }
+    return current;
+  }
 };
 
 // Do not edit below this line
