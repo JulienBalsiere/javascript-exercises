e5cec59857cfff2e194f0aae41ae1140e0c00c25
Julien Balsiere <julien.balsiere@gmail.com>
2026-06-25 11:07:45 +0200

    all exercices done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+// Trouver la personne la plus agée
+const findTheOldest = function(people) {
+  const getAge = function(person) {
+  const birth = person.yearOfBirth;
+  const death = person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
+  return death - birth;
+  };
+
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(currentPerson);
+
+    if (currentAge > oldestAge) {
+      return currentPerson;
+    } else {
+      return oldest;
+    }
+  });
 
 };
 
